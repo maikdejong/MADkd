1f0ff8cbfbf089aec57dbb70241fb3958b40e695
maik <maik-de-jong@live.nl>
2023-05-25 09:58:26 +0200

    Theme switch functionaliteit

--- AppKeuzedeel/App.js
+++ AppKeuzedeel/App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Switch } from "react-native";
+import { ThemeContext } from "./src/context/ThemeContext";
+import { myColors } from "./src/styles/Colors";
 
 export default function App() {
+  const [theme, setTheme] = useState('light');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeContext.Provider value={theme}>
+      <View style={theme === 'light' ? styles.container : [styles.container, {backgroundColor: myColors.black}]}>
+        <Text style={theme === 'dark' ? styles.text : [styles.text, {color: 'black'}]}>Switch Theme</Text>
+        <StatusBar style="auto" />
+        <Switch
+          value={theme === 'light'}
+          onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+          />
+      </View>
+    </ThemeContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: myColors.white,
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    color: 'white',
+  }
 });
